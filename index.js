4dd5a47d5dd41eb6da700d6164ea22a4ef99b01d
arindanbabu <30999026+arindanbabu@users.noreply.github.com>
2026-04-25 12:14:23 +0530

    Update greeting message in index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello CI/CD 🚀");
+  res.send("Hello CI/CD 🚀 IM ARINDAM MONDAL");
 });
 
 app.listen(3000, "0.0.0.0", () => {
