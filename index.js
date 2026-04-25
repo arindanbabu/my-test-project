45b9fca2592b736e92e899af3ca7d9d03deadffc
arindanbabu <30999026+arindanbabu@users.noreply.github.com>
2026-04-25 12:43:55 +0530

    Add home route for CI/CD deployment message
    Added a new route for the home page that responds with a message about the CI/CD deployment.

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello CI/CD 🚀 IM ARINDAM MONDAL Test Deploying VIA CI CD");
 });
-
+app.get("/home", (req, res) => {
+  res.send("This is home route of the repo deployed VIA CI CD");
+});
 app.listen(3000, "0.0.0.0", () => {
   console.log("Server running on port 3000");
 });
